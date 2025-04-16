ca410e9543a15545c7ff8a15ef9020135b629ac2
Jefferson Albuquerque <117997242+JeffersonAlbuquerque@users.noreply.github.com>
2025-04-16 11:37:39 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+import sqlite3
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+def init_db():  # inicia o database.
+    with sqlite3.connect("database.db") as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS CADASTRO(
+                     nome TEXT NOT NULL,
+                     cpf TEXT PRIMARY KEY,
+                     nascimento TEXT NOT NULL,
+                     email TEXT NOT NULL UNIQUE,
+                     celular TEXT NOT NULL,
+                     telefone TEXT,
+                     senha TEXT NOT NULL)
+""")
+
+
+init_db()
+
+
+@app.route("/")
+def bem_vindo():
+    return "<h1>Seja bem vindo!</h1>"
+
+
+@app.route("/login", methods=["POST"])
+def cadastrarUsuario():
+    dados = request.get_json()
+
+    nome = dados.get("nome")
+    cpf = dados.get("cpf")
+    nascimento = dados.get("nascimento")
+    email = dados.get("email")
+    celular = dados.get("celular")
+    telefone = dados.get("telefone")
+    senha = dados.get("senha")
+
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(f"""
+            INSERT INTO CADASTRO (nome, cpf, nascimento, email, celular, telefone, senha)
+            VALUES ("{nome}", "{cpf}", "{nascimento}", "{email}", "{celular}", "{telefone}", "{senha}")
+""")
+        conn.commit() #COMANDO PARA SALVAR AS MUDANÇAS NO BANCO DE DADOS.
+
+        return jsonify({"Mensagem": "Usuário Cadastrado com sucesso"}), 201
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
